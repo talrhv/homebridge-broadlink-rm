2ba751ccbbf3f8dcac299332c8acfae2936145e8
Tal <talrhv1@gmail.com>
2026-07-10 18:13:56 +0300

    try to fix cooling to heating

--- accessories/heater-cooler.js
+++ accessories/heater-cooler.js
@@ -1199,7 +1199,14 @@ class HeaterCoolerAccessory extends BroadlinkRMAccessory {
         setMethod: this.setCharacteristicValue,
         bind: this,
         props: {
-          setValuePromise: this.setTemperature.bind(this),
+          setValuePromise: (async (hexData, previousValue) => {
+  if (this.state.targetHeaterCoolerState !== Characteristic.TargetHeaterCoolerState.HEAT) {
+    this.state.targetHeaterCoolerState = Characteristic.TargetHeaterCoolerState.HEAT;
+    this.serviceManager.setCharacteristic(Characteristic.TargetHeaterCoolerState, Characteristic.TargetHeaterCoolerState.HEAT);
+    this.updateServiceCurrentHeaterCoolerState();
+  }
+  return this.setTemperature(hexData, previousValue);
+}).bind(this),
         }
       })
       // Characteristic properties
